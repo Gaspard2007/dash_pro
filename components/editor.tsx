25abb1211f1793483bde59156be7ad9944d1efc7
Gaspard2007 <124401506+Gaspard2007@users.noreply.github.com>
2024-05-03 22:51:57 +0200

    Update editor.tsx

--- components/editor.tsx
+++ components/editor.tsx
@@ -3,5 +3,55 @@ import dynamic from 'next/dynamic';
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import 'react-quill/dist/quill.snow.css';
 import { Quill } from 'react-quill';
+const Size = Quill.import('attributors/style/size');
+Size.whitelist = fontSizes;
+Quill.register(Size, true);
+
+const Editor = (props) => {
+    const [editorHtml, setEditorHtml] = useState('');
+
+    const handleChange = (html) => {
+        setEditorHtml(html);
+    }
+
+    return (
+        <ReactQuill
+            style={{
+                height: '1200px',
+                width: '1200px',
+                backgroundColor: 'white',
+            }}
+            onChange={handleChange}
+            value={editorHtml}
+            modules={Editor.modules}
+            formats={Editor.formats}
+            placeholder={props.placeholder}
+        />
+    );
+}
+
+Editor.modules = {
+    toolbar: [
+        [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
+        [{ 'size': fontSizes }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' },
+        { 'indent': '-1' }, { 'indent': '+1' }],
+        ['link', 'image', 'video'],
+        ['clean']
+    ],
+    clipboard: {
+        matchVisual: false,
+    }
+}
+
+Editor.formats = [
+    'header', 'font', 'size',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image', 'video'
+]
+
+export default Editor;
 
 
